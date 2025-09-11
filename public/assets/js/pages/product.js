5792185aefad41355b5a25bf68d938bfc1d20850
arianzarifi <zarifiarian37@gmail.com>
2025-09-12 00:07:20 +0330

    comment

--- public/assets/js/pages/product.js
+++ public/assets/js/pages/product.js
@@ -24,3 +24,27 @@ function changeImage(thumbnail) {
         });
     })
   }) ;
+  //comments
+  let modal = document.getElementById('commentModal');
+  let openBtn = document.querySelector('.add-comment-btn');
+  let closeBtn = document.getElementById('closeModal');
+
+  openBtn.addEventListener('click' , () => {
+    modal.style.display = 'flex' ;
+  });
+  closeBtn.addEventListener('click' , () => {
+    modal.style.display = 'none';
+  });
+  window.addEventListener('click' , (e) => {
+    if(e.target === modal){
+      modal.style.display = 'none';
+    }
+  });
+document.getElementById('sendComment').addEventListener('click' , () =>{
+  const text = document.querySelector('textarea').value ;
+  if(text.trim()){
+    console.log("نظر کاربر:" , text) ;
+    modal.style.display = 'none' ;
+  }
+ 
+});
